c4c53565ddf7295b3ee730d36c41c1817af9ac2f
mohanramr <mohanram9300@gmail.com>
2019-10-31 15:14:04 +0530

    Test Demo

---
+++ Terraform/main.tf
@@ -0,0 +1,5 @@
+resource "aws_vpc" "my-vpc" {
+    cidr_block="10.0.0.0/16"
+
+  }
+}
---
+++ Terraform/provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    access_key="AKIAVTYGLPKPE4GIKPBF"
+    secret_key="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"
+    region="us-east-1"
+  
+}
